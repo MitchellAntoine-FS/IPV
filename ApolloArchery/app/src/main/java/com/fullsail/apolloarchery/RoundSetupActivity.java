48fea77f3d258bebd2f2c55459f29ab3ac3b5f92
MitchellAntoine-FS <amitchell3@student.fullsail.edu>
2023-04-25 13:35:18 -0700

    initial commit

---
+++ ApolloArchery/app/src/main/java/com/fullsail/apolloarchery/RoundSetupActivity.java
@@ -0,0 +1,14 @@
+package com.fullsail.apolloarchery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RoundSetupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round_setup);
+    }
+}
---
+++ ApolloArchery/app/src/main/java/com/fullsail/apolloarchery/ShootingActivity.java
@@ -0,0 +1,14 @@
+package com.fullsail.apolloarchery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShootingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shooting);
+    }
+}
